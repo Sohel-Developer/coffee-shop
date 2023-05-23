3158dd0aa62a66df11f3ecabec9486bda9174036
Sohel-Developer <sohel.webdevoloper.com>
2023-05-23 20:05:22 +0600

    Navbar Design Complete

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,8 +1,9 @@
 
+
 const Home = () => {
     return (
         <div>
-            <h2>This is hom</h2>
+            <h2>Home</h2>
         </div>
     );
 };
---
+++ src/Shared/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+
+import './Navbar.css'
+
+import logo from '../../assets/more/logo1.png'
+
+const Navbar = () => {
+    return (
+        <div className="navbar py-4 flex items-center justify-center ">
+            <img src={logo} className='w-16 mr-5' alt="Logo" />
+            <h2 className='text-white text-6xl'>Espresso Emporium</h2>
+
+        </div>
+    );
+};
+
+export default Navbar;

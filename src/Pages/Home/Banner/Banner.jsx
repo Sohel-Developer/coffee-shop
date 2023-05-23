d0462862022529c6e47f10841bc7c362933fa30b
Sohel-Developer <sohel.webdevoloper.com>
2023-05-23 20:25:16 +0600

    Banner Design Done

---
+++ src/Pages/Home/Banner/Banner.jsx
@@ -0,0 +1,15 @@
+
+
+
+const Banner = () => {
+    return (
+        <div className="banner py-60 ">
+            <div className=" text-white ms-auto   w-[600px]">
+                <h2 className="text-5xl pb-5 ">Would you like a Cup <br /> of Delicious Coffee?</h2>
+                <p>It is coffee time - Sip & Savor - Relaxation in every sip! Get the nostalgia back!! <br /> Your companion of every moment!!! Enjoy the beautiful <br /> moments and make them memorable.</p>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;

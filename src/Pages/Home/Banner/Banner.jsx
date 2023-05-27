61df3fa36dff54d9bc62f41e75467523b76a272d
Sohel-Developer <sohel.webdevoloper.com>
2023-05-27 12:29:49 +0600

    Code Refactor

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -3,10 +3,13 @@
 
 const Banner = () => {
     return (
-        <div className="banner py-60 ">
-            <div className=" text-white ms-auto   w-[600px]">
-                <h2 className="text-5xl pb-5 ">Would you like a Cup <br /> of Delicious Coffee?</h2>
-                <p>It is coffee time - Sip & Savor - Relaxation in every sip! Get the nostalgia back!! <br /> Your companion of every moment!!! Enjoy the beautiful <br /> moments and make them memorable.</p>
+        <div className="banner  py-60 ">
+            <div className="max-w-7xl mx-auto">
+                <div className="  text-white ms-auto space-y-4 px-5 md:w-3/5">
+                    <h2 className="text-5xl  font-rancho ">Would you like a Cup  of Delicious Coffee?</h2>
+                    <p className="font-raleway">It is coffee time - Sip & Savor - Relaxation in every sip! Get the nostalgia back!! <br /> Your companion of every moment!!! Enjoy the beautiful <br /> moments and make them memorable.</p>
+                    <button className="py-2 px-5  bg-[#E3B577] text-2xl font-rancho">Learn More</button>
+                </div>
             </div>
         </div>
     );
--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import logo from '../../assets/more/logo1.png'
 
 const Navbar = () => {
     return (
-        <div className="navbar py-4 flex items-center justify-center ">
+        <div className="navbar  py-4 px-5 flex items-center justify-center ">
             <img src={logo} className='w-16 mr-5' alt="Logo" />
             <h2 className='text-white text-6xl'>Espresso Emporium</h2>
 

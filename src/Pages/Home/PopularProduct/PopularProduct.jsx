bc7acfaccff5868813804ee171b14458fadf5c77
Sohel-Developer <sohel.webdevoloper@gmail.com>
2024-05-30 15:37:44 +0600

    update

--- src/Pages/Home/PopularProduct/PopularProduct.jsx
+++ src/Pages/Home/PopularProduct/PopularProduct.jsx
@@ -13,7 +13,7 @@ const PopularProduct = () => {
     const [coffees, setCoffees] = useState([])
 
     useEffect(() => {
-        fetch('https://coffee-shop-server.up.railway.app/coffee')
+        fetch('https://coffee-shop-server-ynia.onrender.com/coffee')
             .then(res => res.json())
             .then(data => {
                 setCoffees(data)
